cb2d9eb6a7cafa4088ad7a02829657c215036698
Lukas Kaul <lukas.kaul@kit.edu>
2022-04-13 08:08:09 +0200

    Implement sliding window velocity estimator.

--- HallEncoder.cpp
+++ HallEncoder.cpp
@@ -17,6 +17,9 @@ HallEncoder::HallEncoder(uint8_t pin_A, uint8_t pin_B, uint8_t pin_Z) {
     hall_state_table_[5] = 0;
     hall_state_table_[6] = 0;
     hall_state_table_[7] = -1; // Invalid Hall state.
+    
+    last_sampling_time_ = micros();
+    last_change_time_us_ = micros();
 }
 
 float HallEncoder::GetVelocity() {
@@ -27,6 +30,19 @@ long int HallEncoder::GetPosition() {
     return position_;
 }
 
+int HallEncoder::GetRawPosition() {
+    return hall_state_table_[last_state_];
+}
+
+int HallEncoder::GetState() {
+    return last_state_;
+}
+
+void HallEncoder::Reset() {
+    position_ = 0;
+}
+
+// Set the calibration table from known calibration values.
 void HallEncoder::WriteCalibration(uint8_t pos_1, uint8_t pos_2, uint8_t pos_3, uint8_t pos_4, uint8_t pos_5, uint8_t pos_6) {
     hall_state_table_[1] = pos_1;
     hall_state_table_[2] = pos_2;
@@ -34,14 +50,6 @@ void HallEncoder::WriteCalibration(uint8_t pos_1, uint8_t pos_2, uint8_t pos_3, 
     hall_state_table_[4] = pos_4;
     hall_state_table_[5] = pos_5;
     hall_state_table_[6] = pos_6;
-}
-    
-int HallEncoder::GetState() {
-    return last_state_;
-}
-
-int HallEncoder::GetRawPosition() {
-    return hall_state_table_[last_state_];
 }
 
 // Blocking calibration routine.
@@ -70,10 +78,36 @@ int HallEncoder::Calibrate() {
     return 0;
 }
 
-void HallEncoder::Reset() {
-    position_ = 0;
+// Update function to be periodically called as often as possible (at higher frequencies than Hall state transition occur).
+// This function does to things:
+// 1) Read the three Hall sensors and update the internal state and raw position (0-5)
+// 2) At fixed time intervals record the position in a sliding window for moving average velocity estimation.
+int HallEncoder::Update() {
+  unsigned long now = micros();
+  // Do this whenver called, hopefully at sufficiently high frequency.
+  uint8_t state = ReadHallState(); // Fingers crossed that the state does not change while we sample it.
+  int state_change = GetStateChange(state); // -1 or 1
+  last_state_ = state;
+  if (abs(state_change) == 1) {
+      // Postion increased or decreased by one.
+      position_ += state_change;
+      // velocity_cps_ = 1000000.0 * (float)state_change / (float)(micros() - last_change_time_us_);
+      last_change_time_us_ = now;
+  }
+  else if (state_change != 0) {
+      // A Hall error occured.
+      return -1;
+  }
+  // Update the velocity estimate as the average velocity over the last kWindowSize_ position samples taken kSamplingIntervalUs_ microseconds spaced apart.
+  if(now >= (last_sampling_time_ + kSamplingIntervalUs_)) {
+      last_sampling_time_ += kSamplingIntervalUs_;
+      oldest_position_ = position_window_[sampling_index_];
+      position_window_[sampling_index_] = position_;
+      sampling_index_ = (sampling_index_ + 1) % kWindowSize_;
+      velocity_cps_ = 1000000.0 * (float)(position_ - oldest_position_)/((float)(kSamplingIntervalUs_ * kWindowSize_));
+  }
+  return 0;
 }
-
 
 uint8_t HallEncoder::ReadHallState() {
   uint8_t a = digitalRead(pin_A_);
@@ -82,57 +116,34 @@ uint8_t HallEncoder::ReadHallState() {
   // a, b and z are the three least significant bits of the result.
   uint8_t state = a << 2 | b << 1 | z;
   if (state < 1 || state > 6) {
-            // Invalid Hall state.
-            Serial.println("Calibration failed. Result may be invalid.");
-            return -1;
-        }
-        return state;
-}
-
-int HallEncoder::Update() {
-  unsigned long now = micros();
-  uint8_t state = ReadHallState(); // Fingers crossed that the state does not change while we sample it.
-  int state_change = GetStateChange(state); // -1 or 1
-  last_state_ = state;
-  if (abs(state_change) == 1) {
-      // Postion increased or decreased by one.
-      position_ += state_change;
-      velocity_cps_ = 1000000.0 * (float)state_change / (float)(micros() - last_change_time_us_);
-      last_change_time_us_ = now;
+    // Invalid Hall state.
+    return 0;
   }
-  else if (state_change != 0) {
-      // A Hall error occured.
-      return -1;
-  }
-  return 0;
+  return state;
 }
 
 /* Compare the current to the previous encoder state.
- * 0: Unchanged
+ * 0: Unchanged or error
  * -1: Decreased
- * 2: Increased
- * All others: Error codes.
+ * 1: Increased
  */
 int HallEncoder::GetStateChange(uint8_t state) {
     // Check for Hall state validity. 
     if (hall_state_table_[last_state_] == -1) {
         // Last hall state is invalid.
-        return 10;
+        return 0;
     }
     if (hall_state_table_[state] == -1) {
         // current hall state is invalid.
-        return 20;
-    }
-    
+        return 0;
+    }    
     int last_pos = hall_state_table_[last_state_];
     int new_pos = hall_state_table_[state];
     // See what changed.
     if (new_pos == (last_pos + 1) % 6) {
-        // Serial.println("Encoder position has increased.");
         return 1;
     }
     else if (((last_pos - 1) % 6) == new_pos || (last_pos == 0 && new_pos == 5)) {
-        // Serial.println("Encoder position has decreased.");
         return -1;
     }
     else if (new_pos == last_pos) {
@@ -145,6 +156,6 @@ int HallEncoder::GetStateChange(uint8_t state) {
         Serial.print(new_pos);
         Serial.print('\t');
         Serial.println("Encoder has skipped at least one step.");
-        return 30;
+        return 0;
     }
 }
--- HallEncoder.h
+++ HallEncoder.h
@@ -15,14 +15,22 @@ class HallEncoder{
   int GetRawPosition();
   
   private:
+  static const unsigned long kWindowSize_ = 20;
+  static const unsigned long kSamplingIntervalUs_ = 10000;
   uint8_t pin_A_ = 0;
   uint8_t pin_B_ = 0;
   uint8_t pin_Z_ = 0;
   uint8_t last_state_ = 0;
   long int position_ = 0;
+  long int oldest_position_ = 0;
+  unsigned long last_sampling_time_ = 0;
   unsigned long last_change_time_us_ = 0;
   float velocity_cps_ = 0;
   int hall_state_table_[8];
+  // Sliding window for moving average velocity estimation.
+  // Using a sliding averge to achieve a better trade-off between low speed accuracy and lag than a single dx/dt with fixed dt. 
+  long int position_window_[kWindowSize_];
+  uint8_t sampling_index_ = 0;
   
   uint8_t ReadHallState();
   int GetStateChange(uint8_t state);
